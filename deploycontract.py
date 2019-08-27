f48b094ffbe3d165c6cb967f6609a459f925582d
michael-free <michaelfree@michaelfree.net>
2019-08-27 01:58:31 +0000

    shrink down contract

--- deploycontract.py
+++ deploycontract.py
@@ -1,3 +1,4 @@
+
 from solc import compile_source
 from web3 import Web3, HTTPProvider
 from web3.contract import ConciseContract
@@ -9,32 +10,15 @@ contract StorageContract {
     /* Define variable owner of the type address */
     string public serialnumber;
     address public assetowner;
-    string public location;
 
     event Registration(
        string serialnumber,
        address assetowner
     );
-
     function setRegistration (string newSerialnumber, address newAssetowner) public {
         serialnumber = newSerialnumber;
         assetowner = newAssetowner;
         emit Registration(serialnumber, assetowner);
-
-    }
-
-    event Reporting(
-       string serialnumber,
-       string location,
-       address assetowner
-    );
-
-    function setReporting (string newSerialnumber, string newLocation, address newAssetowner) public {
-        serialnumber = newSerialnumber;
-        location = newLocation;
-        assetowner = newAssetowner;
-        emit Reporting(serialnumber, location, assetowner);
-
     }
 }
 '''
